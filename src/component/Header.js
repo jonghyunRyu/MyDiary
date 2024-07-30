dbd236c3f7e2d6e7394effeba26333dbdb8bc379
jonghyunRyu <willowise@naver.com>
2024-07-30 17:29:26 +0900

    feat: Header와 Navbar 구현

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,17 @@
+import "./Header.css";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <div className="Header">
-            헤더입니다
+            <Link to="/" className="header_logo">
+                Mydiary
+            </Link>
+            <div className="header_button">
+                <Button className="login" text={"로그인"} type={"positive"}/>
+                <Button className="signup" text={"가입하기"} type={"default"}/>
+            </div>
         </div>
     );
 };
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,20 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className="Navbar">
-            네브바입니다
+            <Link to="/todo" className="Navbar_link">
+                <button>할 일</button>
+            </Link>
+            <Link to="/diary" className="Navbar_link">
+                <button>일기장</button>
+            </Link>
+            <Link to="/post" className="Navbar_link">
+                <button>게시판</button>
+            </Link>
         </div>
     );
 };
 
-export default Navbar;
+export default Navbar;  
